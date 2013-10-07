e0cd35a6f3cf0531f413a2cbeed7e16d4da70f7c
deadbok <martin.groenholdt@gmail.com>
2013-10-07 21:36:43 +0200

    Changed function of the script to dump SMS messages (texts) to text files.

--- README.md
+++ README.md
@@ -1,62 +1,13 @@
-#iPhone SMS Database stats generator#
+#iPhone SMS dump tool#
+This tool is used to dump sms messages from an iPhone backup. Messages are saved as text files named after the address of the person you communicated with.
 
 ##Current Status##
-Currently functioning for frequency generation (ie: how many in/out messages for a specific time period for a specific phone number).
-This script requires the iphone SMS database to be available, and unencrypted.  By default, this database file is called 
-
-	3d0d7e5fb2ce288813306e4d4636395e047a3d28.mddata
-
-and is located in
-
-	~/Library/Application\ Support/MobileSync/Backup/<phone guid>/
-	
-on Mac OS X.  Typical usage assumes a copy of the mddata db file is made in the smsstats.py directory, and called 'sms.db'.  It's probably not
-a good idea to run this against the mddata file in-place, to avoid potentially corrupting/modifying the iPhone backup (the file is MD5 hashed as-is).
+I have no regular access to an iPhone so I only tested this once using an iPhone running IOS 3.1.2 and iTunes version 11.1.1.
 
 ##Usage##
 Options:
 	  -h, --help            show this help message and exit
 	  -d DB_FILE, --db=DB_FILE
-	                        SMS DB File [default: sms.db]
-	  -s STARTTIME, --start=STARTTIME
-	                        DD-MM-YY date to start at [default: 26-05-10]
-	  -e ENDTIME, --end=ENDTIME
-	                        DD-MM-YY date to end at [default: 02-06-10]
-	  -n NUMBER, --number=NUMBER
-	                        Phone # to search with - must match AddressBook.app
-	                        format exactly, in quotes - e.g.: "(888) 555-1212"
-	  -c CHUNKSIZE, --chunksize=CHUNKSIZE
-	                        What size chunk to break the period into, in seconds
-	                        [default: 43200 (12 hours)]
-	  -o OUTPUT, --output=OUTPUT
-	                        What kind of output format? - Console, CSV, TSV
-	                        [default: Console]
-	  -D DATEFORMAT, --dateformat=DATEFORMAT
-	                        stftime style date format [default: %m/%d/%y - %p] (e.g.
-	                        '05/29/10 - AM')
-
-###Example Usage###
-
-####Show default chart for a number####
-
-	python smsstats.py -n "+18885551212"
-	Date: 05/30/10 - AM		 In: 0		Out:0
-	Date: 05/30/10 - PM		 In: 12		Out:10
-	Date: 05/31/10 - AM		 In: 0		Out:1
-	Date: 05/31/10 - PM		 In: 8		Out:4
-	Date: 06/01/10 - AM		 In: 9		Out:6
-	Date: 06/01/10 - PM		 In: 3		Out:3
-
-####Show same number, but with chart broken into 1 hr chunks instead of 12 hour, with a timestamp change####
-	python smsstats.py -n "+18885551212" -c 3600 --dateformat "%y/%m/%d %I%p"
-	Date: 10/05/30 08PM		 In: 0		Out:0
-	Date: 10/05/30 09PM		 In: 32		Out:28
-	Date: 10/05/30 10PM		 In: 9		Out:15
-	Date: 10/05/30 11PM		 In: 2		Out:0
-	Date: 10/05/31 12AM		 In: 24		Out:29
-	Date: 10/05/31 01AM		 In: 12		Out:18
-	Date: 10/05/31 02AM		 In: 0		Out:0
-	Date: 10/05/31 03AM		 In: 0		Out:0
-	Date: 10/05/31 04AM		 In: 0		Out:0
+	                        SMS DB File [default: 3d0d7e5fb2ce288813306e4d4636395e047a3d28.mddata]
 
 
--- smsdump.py
+++ smsdump.py
@@ -1,110 +1,84 @@
 #!/usr/bin/env python
-
-import time, sqlite3, sys, os
-from datetime import date, datetime, timedelta
+from __future__ import print_function
+import time, sqlite3, sys, os, pprint
+from datetime import datetime
 from optparse import OptionParser
 
-def main():
-	
-	parser = OptionParser()
-	parser.add_option("-d", "--db", dest="db_file", help="SMS DB File [default: %default]")
-	parser.add_option("-s", "--start", dest="starttime", help="DD-MM-YY date to start at [default: %default]")
-	parser.add_option("-e", "--end", dest="endtime", help="DD-MM-YY date to end at [default: %default]")
-	parser.add_option("-n", "--number", dest="number", help="Phone # to search with - must match AddressBook.app format exactly, in quotes - e.g.: \"(888) 555-1212\"")
-	parser.add_option("-c", "--chunksize", dest="chunksize", type="int", help="What size chunk to break the period into, in seconds [default: %default (1 hour = 3600 seconds)]")
-	parser.add_option("-o", "--output", dest="output", help="What kind of output format? - Console, CSV, TSV [default: %default]")
-	parser.add_option("-D", "--dateformat", dest="dateformat", help="stftime style date format [default: %default] (e.g. 'Wed 11AM')")
 
-	parser.set_defaults(db_file="sms.db")
-	parser.set_defaults(starttime=date.strftime(date.today() + timedelta(days=-7), "%d-%m-%y"))
-	parser.set_defaults(endtime=date.strftime(date.today(), "%d-%m-%y"))
-	parser.set_defaults(chunksize=43200)
-	parser.set_defaults(output="Console")
-	parser.set_defaults(dateformat="%m/%d/%y - %p")
+def main():
+	"""Read in an apple backup file as a SQL database, and save all messages in the database
+	to text files, naming them after the reciever."""
+
+	parser = OptionParser()
+	parser.add_option("-d", "--db", dest="dbfile", help="SMS DB File [default: %default]")
+
+	#Default name of the sms database file from the iTunes backup
+	parser.set_defaults(dbfile="3d0d7e5fb2ce288813306e4d4636395e047a3d28.mddata")
 
 	(options, args) = parser.parse_args()
-	if not options.number:
-		print "Missing phone number, option REQUIRED."
-		print "Please provide the '-n' or '--number' argument.  See '-h' for help."
+
+	if not os.path.exists(options.dbfile):
+		print("Error: file " + options.dbfile + " does not exist or cannot be found.")
+		print("Please check the file name and try again.")
+		print("See -h for help and all options.")
 		sys.exit(2)
 
-	# Collect all the SMS data
-	data = runsearch(options.db_file, options.starttime, options.endtime, options.number, options.chunksize)
-	# Pass SMS raw data to be formatted for the user-specified output
-	output(data, options.output, options.dateformat)
+	print("Opening: " + options.dbfile)
+	conn = sqlite3.connect(options.dbfile)
+	
+	msgs = ""
+	with conn:
+		#Get dict
+		conn.row_factory = sqlite3.Row
+		cur = conn.cursor()
+
+		#Query to get all records
+		all_query = "SELECT * FROM message"
+
+		#Get all messages
+		try:
+			cur.execute(all_query)
+			msgs = cur.fetchall()
+			
+		except sqlite3.OperationalError, e:
+			print("Error: Something is wrong or unexpected in the sms database file.")
+			print("Are you sure this is a valid, unencrypted iPhone SMS sqlite database?")
+			print("See -h for help and all options.")
+			sys.exit(2)
+	
+	print(str(len(msgs)) + " message(s)")
+	print("")
+	
+	for row in msgs:
+		print(".", end="")
+		if (row["address"] != None) and (row["text"] != None):
+			#Open a text file with the reciever number without space and plus
+			with open(trans_addr(row["address"]) + '.txt', 'a') as f:
+				dir = ""
+				if row["flags"] == 2:
+					dir = "From: "
+				if row["flags"] == 3:
+					dir = "To: "
+				
+				f.write(dir + row["address"] + " ")
+				f.write(str(datetime.fromtimestamp(row["date"])) + "\n\n")
+				f.write(row["text"].encode('utf-8'))
+				f.write("\n\n")
+				
+	
+	print("")
+	print("Done.")
 	# Exit normally
 	sys.exit()
 	
-def output(data, output, dateformat):
-	"""Takes a list lists of raw sms data (each sublist is [date, in, out]), 
-	the output option flag passed on the command line (if any),
-	 and the date format string passed on the command line (if any).
-	No return value - prints directly to STDOUT."""
-	
-	if (output == "CSV") or (output =="csv"):
-		# Pretty print to the console
-		printformat = "%s,%s,%s"
-	elif (output == "TSV") or (output == "tsv"):
-		# Tab seperated
-		printformat = "%s\t%s\t%s"
-	else:
-		#Default to console
-		printformat = "Date: %s\t\t In: %s\t\tOut:%s"
-
-	# data is list of lists.  row is [date, in, out] message counts
-	try:
-		for row in data:
-			print printformat % (time.strftime(dateformat, row[0]), row[1], row[2])
-	except TypeError, e:
-			print "Error: Bad date returned, exiting."
-			sys.exit(2)
-
-def runsearch(dbfile, start, end, number, chunksize):
-	"""Method takes the database filename, start date, end date, phone number, and chunksize in seconds to query with.  Returns a list of lists."""
-	if not os.path.exists(dbfile):
-		print "Error: file '%s' does not exist or cannot be found." % dbfile
-		print "Please check the file name and try again."
-		print "See -h for help and all options."
-		sys.exit(2)
-	
-	conn = sqlite3.connect(dbfile)
-	db = conn.cursor()
-	
-	# SMS DB - flags determine the direction of the message - sent or recieved
-	out_query = "SELECT COUNT(*) FROM message WHERE flags='3' AND address=? AND date > ? AND date < ?"
-	in_query = "SELECT COUNT(*) FROM message WHERE flags='2' AND address=? AND date > ? AND date < ?"
-
-	# this is convoluted, but it takes the user input "dd-mm-yy" format time stamp, converts it to a unix timestamp.  Python datetime doesn't HAVE a 'totimestamp' capability, 
-	# so you have to convert to a time tuple and use the "time" module's mktime() function instead.
-	try:
-		startstamp 	= time.mktime(datetime.strptime(start, "%d-%m-%y").timetuple())
-		endstamp 	= time.mktime(datetime.strptime(end, "%d-%m-%y").timetuple())
-	except ValueError, e:
-		print "Error: Date entered in wrong format.  Should be in 'DD-MM-YY' format."
-		print "Please check the date entered and try again."
-		print "See -h for help and all options."
-		sys.exit(2)
-
-	results = []
-	# converting everything to 'int' to keep xrange happy.
-	try:
-		for x in xrange(int(startstamp), int(endstamp), int(chunksize)):
-			outmsg = db.execute(out_query, (number, x, x+int(chunksize-1)))
-			a = outmsg.fetchall()[0]
-			inmsg = db.execute(in_query, (number, x, x+int(chunksize-1)))
-			b = inmsg.fetchall()[0]
-
-			row = [time.gmtime(x), a[0], b[0]]
-			results.append(row)
-
-	except sqlite3.OperationalError, e:
-		print "Error: Something is wrong or unexpected in the sms database file."
-		print "Are you sure this is a valid, unencrypted iPhone SMS sqlite database?"
-		print "See -h for help and all options."
-		sys.exit(2)
-
-	return results
-
+def trans_addr(addr):
+	"""This function takes an address and returns only alphanumeric charecters for use as a filename."""
+	ret = ""
+	for ch in addr:
+		if ch.isalnum():
+			ret += ch
+			
+	return(ret) 
 
 if __name__ == "__main__":
 	main()
